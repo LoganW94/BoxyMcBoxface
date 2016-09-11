296a8d5a94cb1297bf254ad23c6f6e443ff38e51
LoganW94 <loganwogoman@gmail.com>
2016-09-11 19:40:08 -0400

    began new simple game

---
+++ main.rb
@@ -0,0 +1,29 @@
+require 'gosu'
+
+
+class Window < Gosu::Window
+
+	
+
+	def initialize width = 800, height = 600, fullscreen = false
+		super
+		
+		self.caption = " ****pre pre pre pre prealpha****"
+
+	end
+
+	def update
+
+
+
+
+	end 
+
+	def draw
+
+
+
+	end
+end
+
+Window.new.show
