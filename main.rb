81796bbbca709fdb0e8949ecc30e4b79793c36a9
LoganW94 <loganwogoman@gmail.com>
2016-09-11 23:11:03 -0400

    stuff

--- main.rb
+++ main.rb
@@ -13,6 +13,7 @@ class Window < Gosu::Window
 
 		@state = 0
 		@continue = false
+		@is_player = false
 
 		@new_game_pos = height/3
 		@quit_game_pos = (height/3)*2
@@ -20,8 +21,9 @@ class Window < Gosu::Window
 		@pos_x = width/4
 		@player_pos_x = 150
 		@player_pos_y = 400
-		
+
 		@font = Gosu::Font.new(70)
+		@font_small = Gosu::Font.new(30)
 		@dot = "@"
 		@new_game = "New Game"
 		@resume_game = "Resume Game"
@@ -36,15 +38,19 @@ class Window < Gosu::Window
         @new_press_escape, 
         @new_press_tab = false
 
+        @level_one_code = 0000
+        @level_two_code = 4533
+        #etc
+
 	end
 
 	def update
 
+##############################################################################
 		if @state == 0 && @continue == false# main menu state if no game in progress
 			
 
             if button_down?(Gosu::KbUp) && @new_press_up
-
  				@dot_pos = @new_game_pos
 
             	if @dot_pos == @quit_game_pos
@@ -70,10 +76,24 @@ class Window < Gosu::Window
 					self.close
 				end						
 			end
+##############################################################################
+		elsif @state == 1 # game state			
 
+			if button_down?(Gosu::KbEscape) && @new_press_escape
+				@state = 0
+				@dot_pos = @new_game_pos
+				@new_game = @resume_game
+			end
+
+			if button_down?(Gosu::KbLeft)
+            	@player_pos_x -=5                  
+            end
+
+            if button_down?(Gosu::KbRight)
+                @player_pos_x +=5                                     
+            end
+##############################################################################
 		elsif @state == 0 && @continue == true # main menu if game in progress
-			@new_game = @resume_game
-
 
             if button_down?(Gosu::KbUp) && @new_press_up
 
@@ -91,8 +111,7 @@ class Window < Gosu::Window
             		@dot_pos = @quit_game_pos
             	end            	
 
-            end
-		
+            end		
 			
 			if button_down?(Gosu::KbReturn) && @new_press_enter
 
@@ -101,35 +120,56 @@ class Window < Gosu::Window
 				elsif @dot_pos == @quit_game_pos
 					self.close
 				end						
-			end				
+			end	
+##############################################################################
+		elsif @state == 2 # level select
 
-		elsif @state == 1 # game state
-
-			if button_down?(Gosu::KbEscape) && @new_press_escape
-				@state = 0
-				@dot_pos = @new_game_pos
+			if @is_player == false
+				@player = Player.new
+				@is_player = true
 			end
 
-			if button_down?(Gosu::KbLeft)
-            	@player_pos_x -=5                  
-            end
-
-            if button_down?(Gosu::KbRight)
-                @player_pos_x +=5                                     
-            end
-
-		elsif @state == 2 # Player Creation
-
 			@dot_pos = height/2 - 35
+			@level = 1
+			@display = "Press Enter to play level: #{@level}"
 
-            @player = Player.new
+			
+			@code_entry_one = "0"
+			@code_entry_two = "0"
+			@code_entry_three = "0"
+			@code_entry_four = "0"
+			@code = @code_entry_one + @code_entry_two + @code_entry_three + @code_entry_four
+			@Code_display = "Level Code: #{@code}"
+			
+
+			if button_down?(Gosu::KbLeft) &&  @new_press_left
+                
+            end
+
+            if button_down?(Gosu::KbRight) &&  @new_press_right
+                                   
+            end       
+
+            if button_down?(Gosu::KbUp) && @new_press_up         			
+                                
+            end    
+
+            if button_down?(Gosu::KbDown) && @new_press_down
+            	
+            end	
 				
             if button_down?(Gosu::KbReturn) && @new_press_enter
             	@state = 1
             	@continue = true
 
             end
-			
+
+            if button_down?(Gosu::KbEscape) && @new_press_escape
+				@state = 0
+				@continue = false
+				@dot_pos = @new_game_pos
+			end
+##############################################################################
 		end	
 
 		@new_press_enter = !button_down?(Gosu::KbReturn)
@@ -141,7 +181,7 @@ class Window < Gosu::Window
         @new_press_escape = !button_down?(Gosu::KbEscape)
         @new_press_tab = !button_down?(Gosu::KbTab)		
 
-		print @state
+
 	end 
 
 	def draw
@@ -149,12 +189,13 @@ class Window < Gosu::Window
 		if @state == 0
 			@font.draw("#{@new_game}", @pos_x, @new_game_pos, 1 )
 			@font.draw("#{@quit_game}", @pos_x, @quit_game_pos, 1)
-			@font.draw("#{@dot}", @pos_x - 60, @dot_pos, 1)
+			@font_small.draw("#{@dot}", @pos_x - 25, @dot_pos + 20, 1)
 		elsif @state == 1
 			@player.draw(@player_pos_x, @player_pos_y)
 
 		elsif @state == 2
-			@font.draw("new game", 200, height/2-70, 1)				
+			@font.draw("#{@display}", 25, height/2 - 200, 1)
+			@font.draw("#{@Code_display}", 25, height/2, 1)				
 		end
 
 	end
