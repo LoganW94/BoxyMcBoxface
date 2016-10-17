9153252d8d08ddb714fce0deff63da969aae82bc
LoganW94 <loganwogoman@gmail.com>
2016-10-16 21:57:37 -0400

    clean up

--- levelgenerator.rb
+++ levelgenerator.rb
@@ -7,73 +7,49 @@ class LevelGenerator
 	attr_accessor :level, :player_x, :player_y, :num_enemies, :goal, :width, :height
 
 	def initialize level
-
 		@level = level
-
-		@parse = Parse.new
-
-
 		loadlevel
-
-
 		loadgraphics
-
-
 		generate
-
-
 		populate
-
-
 	end
 
 	def loadlevel
-
 		file_location = "./levels/level_#{@level}.txt"
-
+		@level_hash = {}
 		begin	
 			@level_file = File.open(file_location, "r")
-
-			@level_hash = @parse.parse(@level_file)
-
+			@level_hash = Parse.new.parse(@level_file)
 			@level_file.close
 		rescue => e
 			puts e
 		end
-
 	end
 
 	def loadgraphics
-
 		@tile
 		@goal
-
 	end
 
 	def generate
-
 	end
 
 	def populate
-
 	end
 
 	def interpret 
 =begin
 		breaks up bmp and gives player_start, num_enemies, goal etc
-		red = enemy
-		blue = player_start
-		green = goal
-		black = tile
-		magenta = transparent
-
+		! = enemy
+		@ = player_start
+		? = goal
+		* = tile
+		# = nil
 =end	
 	end
 
 	def draw
-
 		#pass graphics and positions into background class which will handle everything
-
 	end
 	
 end
