796f0cc065984f1fc5ee3da07bfbc8a30f9cbd3b
LoganW94 <loganwogoman@gmail.com>
2016-10-16 21:58:48 -0400

    finished class to parse through level .txt file

--- parse.rb
+++ parse.rb
@@ -8,27 +8,27 @@ class Parse
 			line = file.gets.chomp
 			file_list << line
 		end
-
 		file_hash = {}
 		key_list = []
 		value_list = []
-		file_list.each do |current_line|
+		line_num = 1
+		last_line = file_list.size
+		file_list.each do |current_line|			
 			key = find_key(current_line)
 			key_list << key
-			key_list = clean(key_list)
-			#value = find_value(current_line) 
-			#value_list << value
-			#file_hash[key] = value
+			key_list = clean_keys(key_list)
+			value = find_value(current_line) 
+			value_list << value
 		end
-		puts key_list.inspect
-		#puts file_hash.inspect
+		value_list.delete_at(2)
+		value_list = stack_map(value_list)
+		file_hash = create_hash(key_list, value_list)
 		return file_hash
 	end
 
 	def find_key current_line
 		key = ""
 		current_line.each_char do |x|
-			# replace with better system at some point
 			if x == "#" or x == "*"
 				break
 			end
@@ -41,11 +41,20 @@ class Parse
 	end
 
 	def find_value current_line
-
+		start = false
+		value = ""
+		current_line.each_char do |y|
+			if y == ":" or y == "#" or y == "*"
+				start = true
+			end
+			if start == true and y != "" and y != ":"
+				value += y
+			end
+		end
 		return value
 	end
 
-	def clean list # replace with dynamic code
+	def clean_keys list 
 		size = list.size
 		i = 3
 		if i != size
@@ -55,10 +64,34 @@ class Parse
 		return list
 	end
 
-	def list
-#   searches through the value of "Map:" and finds the player start_x, start_y, 
-
+	def stack_map value_list
+		map_array= []
+		line_num = 0
+		value_list_len = value_list.size.to_i
+		value_list.each do |x|
+			if line_num <= 2
+				line_num +=1
+			end
+			if line_num >= 3
+				map_array << x
+			end	
+		end
+		line_num = 2
+		while line_num < value_list_len
+			value_list.delete_at(2)
+			line_num += 1
+		end		
+		value_list << map_array
+		return value_list
 	end
 
-
+	def create_hash key_list, value_list
+		hash = {}
+		i = 0
+		while i < 3 
+			hash[key_list[i]] = value_list[i]
+			i += 1
+		end
+		return hash
+	end
 end
